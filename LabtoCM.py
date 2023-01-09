7ece6d368636494532dbcdc95edb49e9aadf76fb
BryanKelly13 <110911942+BryanKelly13@users.noreply.github.com>
2023-01-09 11:40:18 -0500

    Add files via upload

---
+++ LabtoCM.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+"""
+This piece of code is based upon Appendix C from Nuclear Physics of Stars written by Illiadis,
+specifically modeling equation C.38 and the discussion that precedes it
+For a reaction of the type A(a,b)B
+
+Equation is as follows:
+
+cos(theta) = gamma + cos(theta') / sqrt(1 + gamma^2 + 2*gamma*cos(theta'))
+
+where theta = lab frame, theta' = CM frame, and gamma is a constant defined as:
+
+gamma = sqrt( (m_a * m_b * E_a) / (m_B(m_b + m_B)*Q + m_B(m_B + m_b - m_a)*E_a) )
+"""
+
+# Defining our constants --> masses, Q value, beam Energy
+# This is for a 49Ti(d,p)50Ti reaction
+
+Q = 8.714  # Q value of the reaction in MeV
+E_a = 16   # in MeV --> laboratory bombarding energy
+m_a = 1875.61294257  # deuteron mass in MeV
+m_b = 938.272088 # proton mass in MeV
+m_B = 46511.08755 # 50Ti mass in MeV --> ~49.94479 amu
+
+RAD_TO_DEG = 180.0 / np.pi
+DEG_TO_RAD = np.pi / 180.0
+
+GAMMA = np.sqrt( (m_a * m_b * E_a) / (m_B*(m_b + m_B)*Q + m_B*(m_B + m_b - m_a)*E_a) )
+
+def labToCM(labAngles):
+    cmAngles = np.linspace(15, 62, 900)
+    CM_angle_list = []
+    lab_list_15 = []
+    lab_list_20 = []
+    lab_list_25 = []
+    lab_list_30 = []
+    lab_list_35 = []
+    lab_list_40 = []
+    lab_list_45 = []
+    lab_list_50 = []
+    lab_list_55 = []
+    lab_list_60 = []
+    for i in cmAngles:
+        theta_prime = i * DEG_TO_RAD
+        cosLabAngle = ( GAMMA + np.cos(theta_prime) ) / ( np.sqrt(1 + GAMMA**2 + 2*GAMMA*np.cos(theta_prime)) )
+        labAngle = np.arccos(cosLabAngle)
+        temp = np.format_float_positional(labAngle * RAD_TO_DEG, 2)
+        newAngle = float(temp)
+        #print("Lab Angle: ", newAngle, "  |   CM Angle: ", i )
+        if int(newAngle) % 5 == 0:
+            if int(newAngle) == 15:
+                lab_list_15.append(newAngle)
+                if len(lab_list_15) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 20:
+                lab_list_20.append(newAngle)
+                if len(lab_list_20) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 25:
+                lab_list_25.append(newAngle)
+                if len(lab_list_25) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 30:
+                lab_list_30.append(newAngle)
+                if len(lab_list_30) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 35:
+                lab_list_35.append(newAngle)
+                if len(lab_list_35) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 40:
+                lab_list_40.append(newAngle)
+                if len(lab_list_40) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 45:
+                lab_list_45.append(newAngle)
+                if len(lab_list_45) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 50:
+                lab_list_50.append(newAngle)
+                if len(lab_list_50) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 55:
+                lab_list_55.append(newAngle)
+                if len(lab_list_55) == 1:
+                    CM_angle_list.append(i)
+            if int(newAngle) == 60:
+                lab_list_60.append(newAngle)
+                if len(lab_list_60) == 1:
+                    CM_angle_list.append(i)
+    return CM_angle_list    
+
+
+def main():
+    labAngles = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+    
+    cmAngles = labToCM(labAngles)
+
+    print("CM angles calculated, outputting values now: ")
+    listlen = len(labAngles)
+    print("Lab         CM")
+    for i in range(listlen):
+        print(labAngles[i], "   ", cmAngles[i])
+
+if __name__ == "__main__":
+    main()
